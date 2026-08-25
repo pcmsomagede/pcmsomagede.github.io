5855b073901dddfadac19e3330ffffb015f7388b
pcmsomagede <pcmsomagede@gmail.com>
2026-08-25 08:59:55 +0700

    Segarkan cache untuk UI baru

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pcm-somagede-v4';
+const CACHE='pcm-somagede-v5';
 const ASSETS=['/','/index.html','/style.css','/script.js','/hero.jpg','/pimpinan-1.png','/pimpinan-2.png','/pimpinan-3.png','/pimpinan-4.png','/pimpinan-5.png','/pimpinan-6.jpeg','/pimpinan-7.jpeg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS).catch(()=>{})).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
