2526bbe6278eb1d12070a473cb005e4dfeba3eac
pcmsomagede <pcmsomagede@gmail.com>
2026-08-25 16:14:06 +0700

    Add PCM media migration manifest scaffold

---
+++ media-manifest.js
@@ -0,0 +1,6 @@
+window.PCM_MEDIA_MANIFEST = {
+  // Migrated assets can be added here as:
+  // 'quran.audio.1': { publicId: 'quran/audio/1', resourceType: 'video' },
+  // 'kultum.slug': { publicId: 'kultum/slug', resourceType: 'video' },
+  // 'arsip.2026.001.pdf': { publicId: 'arsip/surat-masuk/2026/001', resourceType: 'raw' },
+};
